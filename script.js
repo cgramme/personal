fbe4d77d9571b77827880f964563a88317c2a605
Chris Gramme <cgramme36@gmail.com>
2015-05-14 22:41:16 -0400

    sdfg

--- script.js
+++ script.js
@@ -5,21 +5,7 @@ $(document).ready(function(){
 
 	
 
-	function loadXMLDoc(url, elementId){
-		var xmlhttp;
-		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
- 			 xmlhttp=new XMLHttpRequest();
- 		}else{// code for IE6, IE5
- 			 xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
- 		}
-		xmlhttp.onreadystatechange=function(){
-  			if (xmlhttp.readyState==4 && xmlhttp.status==200){
-    				document.getElementById(elementId).innerHTML=xmlhttp.responseText;
-   			 }
-  		}
-		xmlhttp.open("GET",url,true);
-		xmlhttp.send();
-	}
+
 
 	$('.projects').click(function() {
   	loadXMLDoc("projects.txt","change-content");
@@ -46,4 +32,20 @@ $(document).ready(function(){
 $(document).on('click', '.android', function () {
     loadXMLDoc("android.txt","change-content");
 });
+
+function loadXMLDoc(url, elementId){
+		var xmlhttp;
+		if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
+ 			 xmlhttp=new XMLHttpRequest();
+ 		}else{// code for IE6, IE5
+ 			 xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+ 		}
+		xmlhttp.onreadystatechange=function(){
+  			if (xmlhttp.readyState==4 && xmlhttp.status==200){
+    				document.getElementById(elementId).innerHTML=xmlhttp.responseText;
+   			 }
+  		}
+		xmlhttp.open("GET",url,true);
+		xmlhttp.send();
+	}
 
